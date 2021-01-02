862e278565419e6652c008a96be58756cc36683b
Matt Pfeiffer <mattpfeiffer54@gmail.com>
2021-01-02 11:01:23 -0600

    Initial Commit

---
+++ Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Matt Pfeiffer on 1/2/21.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
